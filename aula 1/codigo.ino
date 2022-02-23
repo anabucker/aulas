1f264eaa0a236e249e2a71ee3871842037a248ff
anabucker <79269960+anabucker@users.noreply.github.com>
2022-02-23 12:52:18 -0300

    Create codigo.ino

---
+++ aula 1/codigo.ino
@@ -0,0 +1,21 @@
+// C c++ code
+//
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT)              
+}
+
+void loop()
+{
+  digitalWrite(3, HIGH);
+  delay(200); 
+  digitalWrite(3, LOW);
+  delay(200); 
+  digitalWrite(3, HIGH);
+  delay(200); 
+  digitalWrite(3, LOW);
+  delay(200); 
+}
+  
+}
